3bd737c3d3bd3755a4fedc7c72a4100b324a6598
Reid Mason <maddogshain132@gmail.com>
2022-12-04 14:52:56 +0000

    Completed day 4

---
+++ day-4/part-2/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type AssignedRange struct {
+	min int
+	max int
+}
+
+func main() {
+	rawInput, err := getInput()
+	if err != nil {
+		log.Fatal("Failed to get input data")
+	}
+
+	result := getPairsThatFullyContain(rawInput)
+	log.Print(result)
+}
+
+func getPairsThatFullyContain(input string) int {
+	pairsThatFullyContain := 0
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		pairs := strings.Split(line, ",")
+		elf1 := parseSectionRange(pairs[0])
+		elf2 := parseSectionRange(pairs[1])
+
+		elf1ContainsElf2 := elf1.max >= elf2.min && elf1.min <= elf2.min
+		elf2ContainsElf1 := elf2.max >= elf1.min && elf2.min <= elf1.min
+		if elf1ContainsElf2 || elf2ContainsElf1 {
+			pairsThatFullyContain++
+		}
+	}
+
+	return pairsThatFullyContain
+}
+
+func parseSectionRange(sectionRangeString string) AssignedRange {
+	sectionRange := strings.Split(sectionRangeString, "-")
+	min, _ := strconv.Atoi(sectionRange[0])
+	max, _ := strconv.Atoi(sectionRange[1])
+	return AssignedRange{min: min, max: max}
+}
+
+func getInput() (string, error) {
+	data, err := os.ReadFile("input.txt")
+	return string(data), err
+}
