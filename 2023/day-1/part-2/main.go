f0db0a1a77529f4e866348253bcfe7fdbe89f131
Reid Mason <maddogshain132@gmail.com>
2023-12-01 22:41:51 +0000

    Cleaned up day 1 part 2 solution

--- 2023/day-1/part-2/main.go
+++ 2023/day-1/part-2/main.go
@@ -8,7 +8,48 @@ import (
 	"strings"
 )
 
-var numberWords = [...]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+var numberWords = [...]struct {
+	digit string
+	value byte
+}{
+	{
+		digit: "one",
+		value: 1,
+	},
+
+	{
+		digit: "two",
+		value: 2,
+	},
+	{
+		digit: "three",
+		value: 3,
+	},
+	{
+		digit: "four",
+		value: 4,
+	},
+	{
+		digit: "five",
+		value: 5,
+	},
+	{
+		digit: "six",
+		value: 6,
+	},
+	{
+		digit: "seven",
+		value: 7,
+	},
+	{
+		digit: "eight",
+		value: 8,
+	},
+	{
+		digit: "nine",
+		value: 9,
+	},
+}
 
 func main() {
 	data, err := os.ReadFile("input.txt")
@@ -19,6 +60,73 @@ func main() {
 	res := getInputValueSum(input)
 	log.Println(res)
 }
+
+func getInputValueSumV2(input string) int {
+	total := 0
+	lines := strings.Split(strings.TrimRight(input, "\n\r"), "\n")
+
+	for _, line := range lines {
+		firstDigit := getFirstDigitV2(line)
+		lastDigit := getLastDigitV2(line)
+		digit := int(firstDigit*10 + lastDigit)
+
+		total += digit
+	}
+
+	return total
+}
+
+func getFirstDigitV2(line string) byte {
+	firstWordIndex := len(line)
+	var digitValue byte = 0
+	for _, word := range numberWords {
+		pos := strings.Index(line, word.digit)
+		if pos == -1 {
+			continue
+		}
+
+		if pos < firstWordIndex {
+			firstWordIndex = pos
+			digitValue = word.value
+		}
+	}
+
+	for i := 0; i < firstWordIndex; i++ {
+		c := line[i]
+		if c <= 57 {
+			return c - '0'
+		}
+	}
+
+	return digitValue
+}
+
+func getLastDigitV2(line string) byte {
+	lastWordIndex := 0
+	var digitValue byte = 0
+	for _, word := range numberWords {
+		pos := strings.LastIndex(line, word.digit)
+		if pos == -1 {
+			continue
+		}
+
+		if pos > lastWordIndex {
+			lastWordIndex = pos
+			digitValue = word.value
+		}
+	}
+
+	for i := len(line) - 1; i >= lastWordIndex; i-- {
+		c := line[i]
+		if c <= 57 {
+			return c - '0'
+		}
+	}
+
+	return digitValue
+}
+
+var numberWords1 = [...]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 
 func getInputValueSum(input string) int {
 	total := 0
@@ -51,7 +159,7 @@ func getFirstDigit(line string) string {
 
 	firstWordIndex := len(line)
 	firstWord := 0
-	for i, word := range numberWords {
+	for i, word := range numberWords1 {
 		pos := strings.Index(line, word)
 		if pos == -1 {
 			continue
@@ -83,7 +191,7 @@ func getLastDigit(line string) string {
 
 	lastWordIndex := -1
 	lastWord := 0
-	for i, word := range numberWords {
+	for i, word := range numberWords1 {
 		pos := strings.LastIndex(line, word)
 		if pos == -1 {
 			continue
--- 2023/day-1/part-2/main_test.go
+++ 2023/day-1/part-2/main_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"log"
+	"os"
+	"testing"
+)
 
 func TestGetCalibrationsValueSum(t *testing.T) {
 	input := `two1nine
@@ -9,14 +13,35 @@ abcone2threexyz
 xtwone3four
 4nineeightseven2
 zoneight234
-7pqrstsixteen`
-	input = "1fivethree4two5threetmm"
+7pqrstsixteen
+`
 
 	expected := 281
-	expected = 13
 	result := getInputValueSum(input)
 
 	if result != expected {
 		t.Errorf("Expected: %d found: %d", expected, result)
 	}
+}
+
+func loadInput() string {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(data)
+}
+
+func BenchmarkGetCalibrationsValueSumV2(b *testing.B) {
+	input := loadInput()
+
+	b.ResetTimer()
+	getInputValueSumV2(input)
+}
+
+func BenchmarkGetCalibrationsValueSum(b *testing.B) {
+	input := loadInput()
+
+	b.ResetTimer()
+	getInputValueSum(input)
 }
