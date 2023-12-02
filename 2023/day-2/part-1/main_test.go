4dd03576da72b5b6c93914178909df19a20866ab
Reid Mason <maddogshain132@gmail.com>
2023-12-02 17:21:38 +0000

    Added benchmarks

--- 2023/day-2/part-1/main_test.go
+++ 2023/day-2/part-1/main_test.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"log"
+	"os"
 	"testing"
 )
 
-func TestGetCalibrationsValueSum(t *testing.T) {
+func loadInput() string {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(data)
+}
+
+func TestProcess(t *testing.T) {
 	input := `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
@@ -18,4 +28,11 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 	if result != expected {
 		t.Errorf("Expected: %d found: %d", expected, result)
 	}
+}
+
+func BenchmarkProcess(b *testing.B) {
+	input := loadInput()
+
+	b.ResetTimer()
+	process(input)
 }
