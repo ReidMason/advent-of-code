7b420b417ca69bc110dd82ce175b2a2aac867418
Reid Mason <maddogshain132@gmail.com>
2023-12-04 12:47:25 +0000

    Completed day 4 part 1

---
+++ 2023/day-4/part-1/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+type Game struct {
+	id, red, gree, blue int
+}
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	input := string(data)
+	res := process(input)
+	log.Println(res)
+}
+
+var idxs = []int{-1, 1}
+
+func process(input string) int {
+	lines := strings.Split(input, "\n")
+
+	total := 0
+	for _, line := range lines {
+		total += parseLine(line)
+	}
+
+	return total
+}
+
+func parseLine(line string) int {
+	line += " "
+	pos := strings.Index(line, ":") + 2
+
+	score := 0
+	winningNums := make([]string, 0)
+	gettingWinners := true
+	currNum := ""
+	for pos < len(line) {
+		c := line[pos]
+		if c >= '0' && c <= '9' {
+			currNum += string(c)
+		} else if c == ' ' && len(currNum) > 0 {
+			if gettingWinners {
+				winningNums = append(winningNums, currNum)
+			} else if contains(winningNums, currNum) {
+				if score == 0 {
+					score = 1
+				} else {
+					score *= 2
+				}
+			}
+			currNum = ""
+		} else if c == '|' {
+			gettingWinners = false
+		}
+
+		pos++
+	}
+
+	return score
+}
+
+func contains(slice []string, target string) bool {
+	for _, s := range slice {
+		if s == target {
+			return true
+		}
+	}
+
+	return false
+}
