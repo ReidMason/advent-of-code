8c44038ce6a547fa0889d415a42c916234c79612
Reid Mason <maddogshain132@gmail.com>
2024-12-02 00:28:46 +0000

    Simplified solution to 2024 day-1 part-1

--- 2024/day-1/part-1.hs
+++ 2024/day-1/part-1.hs
@@ -1,61 +1,20 @@
-import Data.Text
+import Data.List (sort)
+import Data.Text (pack, splitOn, unpack)
 
 main :: IO ()
 main = do
-  input <- readFile "input.txt"
+    input <- readFile "input.txt"
+    let inputs = map (map read . split) (lines input)
+    let (left, right) = getColumns inputs
+    let total = sum (zipWith (curry diff) (sort left) (sort right))
 
-  let lines = splitLines input
-  let leftColumn = Prelude.map getLeftColumn lines
-  let rightColumn = Prelude.map getRightColumn lines
+    print total
 
-  let total = process leftColumn rightColumn 0
-  print total
+split :: String -> [String]
+split text = Prelude.map unpack (splitOn (pack "   ") (pack text))
 
-process :: [Integer] -> [Integer] -> Integer -> Integer
-process [] [] total = total
-process leftColumn rightColumn total = do
-  let lowestLeftIndex = getLowestIndex leftColumn 0 (-1) (-1)
-  let lowestLeft = leftColumn !! fromIntegral lowestLeftIndex
+diff :: (Integer, Integer) -> Integer
+diff (x, y) = abs (x - y)
 
-  let lowestRightIndex = getLowestIndex rightColumn 0 (-1) (-1)
-  let lowestRight = rightColumn !! fromIntegral lowestRightIndex
-
-  let diff = difference lowestLeft lowestRight
-
-  let newLeftColumn = removeAt leftColumn lowestLeftIndex
-  let newRightColumn = removeAt rightColumn lowestRightIndex
-
-  process newLeftColumn newRightColumn (total + diff)
-
-removeAt :: [a] -> Integer -> [a]
-removeAt [] _ = []
-removeAt (x : xs) index
-  | index == 0 = xs
-  | otherwise = x : removeAt xs (index - 1)
-
-difference :: Integer -> Integer -> Integer
-difference x y = max x y - min x y
-
-getLowestIndex :: [Integer] -> Integer -> Integer -> Integer -> Integer
-getLowestIndex [] _ _ lowestIndex = lowestIndex
-getLowestIndex arr index (-1) (-1) = getLowestIndex (Prelude.tail arr) (index + 1) (Prelude.head arr) 0
-getLowestIndex arr index lowest lowestIndex =
-  do
-    let target = Prelude.head arr
-    if target < lowest
-      then getLowestIndex (Prelude.tail arr) (index + 1) target index
-      else getLowestIndex (Prelude.tail arr) (index + 1) lowest lowestIndex
-
-getLeftColumn :: String -> Integer
-getLeftColumn "" = 0
-getLeftColumn x = read (Prelude.head (getColumns x)) :: Integer
-
-getRightColumn :: String -> Integer
-getRightColumn "" = 0
-getRightColumn x = read (Prelude.last (getColumns x)) :: Integer
-
-getColumns :: String -> [String]
-getColumns x = Prelude.map unpack (splitOn (pack "  ") (pack x))
-
-splitLines :: String -> [String]
-splitLines x = Prelude.map unpack (splitOn (pack "\n") (pack x))
+getColumns :: [[Integer]] -> ([Integer], [Integer])
+getColumns = foldr (\[x, y] (xs, ys) -> (x : xs, y : ys)) ([], [])
