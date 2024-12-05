598cc019a9578e04d8913d34f3ec2cb4c6dfb2e7
Reid Mason <maddogshain132@gmail.com>
2024-12-05 18:32:38 +0000

    Completed 2024 day-5 part-2

--- 2024/day-5/main.go
+++ 2024/day-5/main.go
@@ -13,7 +13,7 @@ func main() {
 		panic(err)
 	}
 
-	result := Day5Part1(string(input))
+	result := Day5Part2(string(input))
 	fmt.Println(result)
 }
 
@@ -41,7 +41,42 @@ func Day5Part1(input string) int {
 		}
 
 		numbers := strings.Split(line, ",")
-		if isOrderValid(orderRules, numbers) {
+		_, invalid := isOrderValid(orderRules, numbers)
+		if !invalid {
+			score += getMidpoint(numbers)
+		}
+		pos++
+	}
+
+	return score
+}
+
+func Day5Part2(input string) int {
+	lines := strings.Split(input, "\n")
+
+	// Get rules
+	orderRules := make([][]string, 0)
+	pos := 0
+	for i, line := range lines {
+		pos = i
+		if line == "" {
+			break
+		}
+		orderRules = append(orderRules, strings.Split(line, "|"))
+	}
+
+	// Get print orders
+	score := 0
+	for pos < len(lines) {
+		line := lines[pos]
+		if line == "" {
+			pos++
+			continue
+		}
+
+		numbers := strings.Split(line, ",")
+		numbers, changed := isOrderValid(orderRules, numbers)
+		if changed {
 			score += getMidpoint(numbers)
 		}
 		pos++
@@ -59,15 +94,34 @@ func getMidpoint(numbers []string) int {
 	return number
 }
 
-func isOrderValid(orderRules [][]string, numbers []string) bool {
-	for _, orderRule := range orderRules {
+func isOrderValid(orderRules [][]string, numbers []string) ([]string, bool) {
+	invalid := false
+	for i := 0; i < len(orderRules); i++ {
+		orderRule := orderRules[i]
 		firstIndex, secondIndex := getIndex(orderRule[0], orderRule[1], numbers)
 		if firstIndex != -1 && secondIndex != -1 && firstIndex > secondIndex {
-			return false
+			numbers = remove(numbers, secondIndex)
+			numbers = append(numbers, orderRule[1])
+			invalid = true
+			i = 0
 		}
 	}
 
-	return true
+	return numbers, invalid
+}
+
+func insert(array []string, value string, index int) []string {
+	fmt.Println(array, value, index)
+	return append(array[:index], append([]string{value}, array[index:]...)...)
+}
+
+func remove(array []string, index int) []string {
+	return append(array[:index], array[index+1:]...)
+}
+
+func move(array []string, srcIndex int, dstIndex int) []string {
+	value := array[srcIndex]
+	return insert(remove(array, srcIndex), value, dstIndex)
 }
 
 func getIndex(first, second string, numbers []string) (int, int) {
